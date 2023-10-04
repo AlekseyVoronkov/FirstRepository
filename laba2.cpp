0f19b090b5d86ea21d235f9373055beb71617462
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2023-10-04 18:28:02 +0700

    Update laba2.cpp

--- laba2.cpp
+++ laba2.cpp
@@ -2,7 +2,7 @@
 * Автор:   Воронков.А.А                                  *
 * Дата:    20.09.2023                                    *
 * Название: Циклы с пред и пост условием, вариант 4      * 
-* https://onlinegdb.com/monmmwULe                        *
+* https://onlinegdb.com/hdqSemKILC                        *
 **********************************************************/
 #include <iostream>
 
@@ -13,9 +13,12 @@ using namespace std;
 int main() {
     float U, g, d, a, y;
     
-    g = 9.80665;
-    a = 0.074;
-    y = 1;
+    cout << "Введите g = ";
+    cin >> g;
+    cout << "Введите y = ";
+    cin >> y;
+    cout << "Введите a = ";
+    cin >> a;
     
     while (d < 0.5) {
         d += 0.1;
