377304746dd2187911a5a8fb0bb22ea4922e6a94
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2023-10-04 18:21:47 +0700

    Update laba2.cpp

--- laba2.cpp
+++ laba2.cpp
@@ -2,9 +2,10 @@
 * Автор:   Воронков.А.А                                  *
 * Дата:    20.09.2023                                    *
 * Название: Циклы с пред и пост условием, вариант 4      * 
-* https://onlinegdb.com/evI0Qeyw7P                        *
+* https://onlinegdb.com/monmmwULe                        *
 **********************************************************/
 #include <iostream>
+
 #include <cmath>
 
 using namespace std;
@@ -12,19 +13,23 @@ using namespace std;
 int main() {
     float U, g, d, a, y;
     
-    d = 0.1;
-    cout << "Введите a = ";
-    cin >> a;
-    cout << "Введите g = ";
-    cin >> g;
-    cout << "Введите y = ";
-    cin >> y;
+    g = 9.80665;
+    a = 0.074;
+    y = 1;
     
-    while (d < 3) {
-        U = sqrt(((g * d) / 2) + ((2 * g * a) / (y * d)));
-        cout << "d = " << d << " U = " << U << "     ";
+    while (d < 0.5) {
         d += 0.1;
+        U = sqrt( ( (g * d) / 2) + ( (2 * g * a) / (y * d) ) );
+        cout << "d = " << d << " U = " << U << endl;
     }
+    
+    do {
+        d += 0.5;
+        U = sqrt( ( (g * d) / 2) + ( (2 * g * a) / (y * d) ) );
+        cout << "d = " << d << " U = " << U << endl;
+    }while (d < 3);
 
     return 0;
 }
+
+
