1eabf1ceb19b02034414cb98aa8f29a091de3148
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2023-10-04 16:53:07 +0800

    First Laba, last changes i hope  .cpp

--- main .cpp
+++ main .cpp
@@ -2,9 +2,11 @@
 * Автор:   Воронков.А.А                        *
 * Дата:    20.09.2023                          *
 * Название: Линейные алгоритмы, вариант 4      * 
-* https://onlinegdb.com/00NC-L5gl6             *
+* https://onlinegdb.com/yCSE3rwdp             *
 ************************************************/
+
 #include <iostream>
+
 #include <cmath>
 
 using namespace std;
@@ -23,15 +25,18 @@ int main() {
     cout << "Введите T2: ";
     cin >> T2;
     
-    K = (y - 1) / y;
+    K = (y-1)/y;
     n1 = 1 / b;
-    n2 = ((T2 - T1) / (T2 + ((T2 - T1) / ((y - 1) * log(a)))));
-    n3 = 1 - K * (log(b) / (pow(b, K) -1));
+    n2 = ( (T2 - T1) / (T2 + ( (T2 - T1) / ( (y - 1) * log(a) ) ) ) ) ;
+    n3 = 1 - K * ((log(b)) / (pow(b, K) -1));
     
-    cout << " a) - " << 1 - pow(n1, K)
-    << " b) - " << n2 << " в) - " << n3;
+    
+    cout << " a) - " << 1 - pow(n1, K) 
+         << " b) - " << n2 
+         << " в) - " << n3;
 
     return 0;
 }
+
 
 
